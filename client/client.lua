141df3fcc9ecf74dbeed712178e6958abc5b977a
davidxd33 <dponce@gmail.com>
2022-12-06 15:51:43 -0500

    refactor(style): Formatting, events, tables

--- client/client.lua
+++ client/client.lua
@@ -1,46 +1,47 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local function toggleNuiFrame(shouldShow)
-  SetNuiFocus(shouldShow, shouldShow)
-  SendReactMessage('setVisible', shouldShow)
+    SetNuiFocus(shouldShow, shouldShow)
+    SendReactMessage('setVisible', shouldShow)
 end
 
-
 RegisterNetEvent('rs-multijob:client:toggleUI', function()
-  toggleNuiFrame(true)
+    toggleNuiFrame(true)
 end)
 
 RegisterNUICallback('getAllSharedJobs', function(data, cb)
-  QBCore.Functions.TriggerCallback('qw-multijob:server:getAllNonWhitelistedSharedJobs', function(result)
-    cb(result)
-  end)
+    QBCore.Functions.TriggerCallback('rs-multijob:server:getAllNonWhitelistedSharedJobs', function(result)
+        cb(result)
+    end)
 end)
 
 RegisterNUICallback('getCurrentPlayerJobs', function(data, cb)
-  QBCore.Functions.TriggerCallback('qw-multijob:server:getCurrentPlayerJobs', function(result)
-    cb(result)
-  end)
+    QBCore.Functions.TriggerCallback('rs-multijob:server:getCurrentPlayerJobs', function(result)
+        cb(result)
+    end)
 end)
 
 RegisterNUICallback('setPlayerJob', function(data, cb)
-  local job = data.job
-  local type = data.type
-  local rank = data.rank
+    local job = data.job
+    local type = data.type
+    local rank = data.rank
 
-  if type == 'nonWhitelisted' then
-    TriggerServerEvent('qw-multijob:server:setPlayerUnwhitelistedJob', job)
-    QBCore.Functions.Notify('Job has been Updated to: '..job, 'success', 7500)
-    cb({})
-  else 
-    TriggerServerEvent('qw-multijob:server:setPlayerWhitelistedJob', job, rank)
-    QBCore.Functions.Notify('Job has been Updated to: '..job, 'success', 7500)
-    cb({})
-  end
+    if type == 'nonWhitelisted' then
+        TriggerServerEvent('rs-multijob:server:setPlayerUnwhitelistedJob', job)
+        QBCore.Functions.Notify('Job has been Updated to: '..job, 'success', 7500)
+
+        cb({})
+    else 
+        TriggerServerEvent('rs-multijob:server:setPlayerWhitelistedJob', job, rank)
+        QBCore.Functions.Notify('Job has been Updated to: '..job, 'success', 7500)
+
+        cb({})
+    end
 end)
 
 RegisterNUICallback('hideFrame', function(_, cb)
-  toggleNuiFrame(false)
-  debugPrint('Hide NUI frame')
-  cb({})
+    toggleNuiFrame(false)
+    debugPrint('Hide NUI frame')
+    cb({})
 end)
 
